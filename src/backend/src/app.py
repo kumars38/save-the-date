cab5240e4b9a99bdc725a5c5358e8015b5b1496f
kumars38 <kumars38@mcmaster.ca>
2022-03-23 05:54:51 -0400

    Add flask support

--- src/backend/src/app.py
+++ src/backend/src/app.py
@@ -1,10 +1,21 @@
 ## This part will be changed to interact with frontend instead of hardcode
 
+import time ##
+from flask import Flask
 from scraper import PDFScraper
 import os
+
+app = Flask(__name__)
 
 dirName = os.path.dirname(__file__)
 filePath = os.path.join(dirName, '../input/two_tables_word.pdf') # Changes depending on test case
 
 p = PDFScraper()
-p.scrape(filePath)
+scrapedData = p.scrape(filePath)
+
+@app.route('/data')
+def get_current_data():
+    return {'scrapedData': scrapedData}
+
+
+
